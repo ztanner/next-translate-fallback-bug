67017fe482962054efa529f58e8588a50518d594
Zack Tanner <zacktanner@gmail.com>
2021-04-21 17:00:49 -0700

    fix index route

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
-import useTranslation from 'next-translate/useTranslation'
-
-import Header from '../components/header'
 
 export default function Home() {
-  const { t } = useTranslation('home')
-
   return (
     <>
-      <Link href="/nested/1">
-        <a>View Bug URL</a>
-      </Link>
+        <a href="/nested/3">View Bug URL</a>
     </>
   )
 }
